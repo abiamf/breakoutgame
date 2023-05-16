f34c504147a35a0cce35871b925d0145a415258b
abiamf <anabeatrizmferraz1@outlook.com>
2023-05-16 02:00:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 let canvas = document.getElementById('game'),
-  ctx = canvas.getContext ('2d'),
-  ballRadius = 9,
-  x = canvas.width / (Math.floor(Math.random() * Math.random() * 10) + 3),
-  y = canvas.height -40,
-  dx = 2;
-  dy = -2;
+    ctx = canvas.getContext ('2d'),
+    ballRadius = 9,
+    x = canvas.width / (Math.floor(Math.random() * Math.random() * 10) + 3),
+    y = canvas.height - 40,
+    dx = 2,
+    dy = -2;
 
 let paddleHeight = 12, 
-  paddleWidth = 72; 
-
+    paddleWidth = 72; 
 
 let paddleX = (canvas.width - paddleWidth ) / 2;
 
@@ -19,32 +18,31 @@ let rowCount = 5,
     brickPadding = 12,
     topOffset = 40,
     letfOffset = 33,
-    score =0;
-
+    score = 0;
 
 let bricks = [];
 for(let c = 0; c < columnCount; c++){
     bricks [c] = [];
     for (let r = 0; r < rowCount; r++){
-      bricks[c][r] = { x: 0, y: 0, status: 1};
+        bricks[c][r] = { x: 0, y: 0, status: 1};
     }
 }
 
 document.addEventListener("mousemove", mouseMoveHandler, false);
 
 function mouseMoveHandler(e){
-    var relaviteX = e.clientX - canvas.offsetLeft; 
+    let relaviteX = e.clientX - canvas.offsetLeft; 
     if(relaviteX > 0 && relaviteX < canvas.width){
-      paddleX = relaviteX - paddleWidth / 2;
+        paddleX = relaviteX - paddleWidth / 2;
     }
 }
 
 function drawPaddle(){
-  ctx.beginPath();
-  ctx.roundRect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight, 30);
-  ctx.fillStyle = '#534f52';
-  ctx.fill();
-  ctx.closePath();
+    ctx.beginPath();
+    ctx.roundRect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight, 30);
+    ctx.fillStyle = '#534f52';
+    ctx.fill();
+    ctx.closePath();
 }
 
 function drawBall(){
@@ -58,73 +56,88 @@ function drawBall(){
 function drawBricks(){
     for(let c = 0; c < columnCount; c++){
         for(let r = 0; r < rowCount; r++){
-        if(bricks[c][r].status === 1){
-            let brickX = (c * (brickWidth + brickPadding)) + letfOffset;
-            let brickY = (r * (brickHeight + brickPadding)) + topOffset;
-            bricks[c][r].x = brickX;
-            bricks[c][r].y = brickY;
-            ctx.beginPath();
-            ctx.roundRect(brickX, brickY, brickWidth, brickHeight, 30);
-            ctx.fillStyle = '#71797E';
-            ctx.fill();
-            ctx.closePath();
-          }
+            if(bricks[c][r].status === 1){
+                let brickX = (c * (brickWidth + brickPadding)) + letfOffset;
+                let brickY = (r * (brickHeight + brickPadding)) + topOffset;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.roundRect(brickX, brickY, brickWidth, brickHeight, 30);
+                ctx.fillStyle = '#71797E';
+                ctx.fill();
+                ctx.closePath();
+            }
         }
     }
 }
 
 function trackScore(){
-  ctx.font = 'bold 16px sans-serif';
-  ctx.fillStyle = '#00755E';
-  ctx.fillText('Score: ' + score, 8,24);
+    ctx.font = 'bold 16px sans-serif';
+    ctx.fillStyle = '#00755E';
+    ctx.fillText('Score: ' + score, 8,24);
 }
 
 function hitDetection() {
-  for (let c = 0; c < columnCount; c++) {
-    for (let r = 0; r < rowCount; r++) {
-      let b = bricks[c][r];
-      if (b.status === 1) {
-        if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
-          dy = -dy;
-          b.status = 0;
-          score++;
-          if (score === rowCount * columnCount) {
-            setTimeout(function () {
-              alert('You Win!');
-              document.location.reload();
-            }, 50);
-          }
+    for (let c = 0; c < columnCount; c++) {
+        for (let r = 0; r < rowCount; r++) {
+            let b = bricks[c][r];
+            if (b.status === 1) {
+                if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                    score++;
+                    if (score === rowCount * columnCount) {
+                        setTimeout(function () {
+                            alert('You Win!');
+                            document.location.reload();
+                        }, 50);
+                    }
+                }
+            }
         }
-      }
     }
-  }
 }
 
-setInterval(init, 15);
+function showGameOver() {
+  const gameOverElement = document.createElement('div');
+  gameOverElement.id = 'game-over';
+  gameOverElement.style.display = 'none';
+  gameOverElement.innerHTML = `
+    <div>
+      <h2>Fim de jogo!</h2>
+      <p>Sua pontuação final foi: <span id="final-score"></span></p>
+      <button onclick="restart()">Jogar novamente</button>
+    </div>
+  `;
+  canvas.parentNode.appendChild(gameOverElement);
+}
 
+
+function restart() {
+  
+  score = 0;
+  x = canvas.width / (Math.floor(Math.random() * Math.random() * 10) + 3);
+  y = canvas.height -40;
+  paddleX = (canvas.width - paddleWidth ) / 2;
+  bricks = [];
+  for(let c = 0; c < columnCount; c++){
+    bricks[c] = [];
+    for (let r = 0; r < rowCount; r++){
+      bricks[c][r] = { x: 0, y: 0, status: 1};
+    }
+  }
+  gameOverElement.style.display = 'none';
+}
 
 function init(){
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  trackScore();
-  drawBricks();
-  drawBall();
-  drawPaddle();
-  hitDetection();
+  
 
-  if(x + dx > canvas.width - ballRadius || x + dx < ballRadius){
-     dx = -dx;
-  }
-
-  if(y + dy < ballRadius){
+  if(y + dy > canvas.height - ballRadius || y + dy < ballRadius){
     dy = -dy;
-  }else if(y + dy > canvas.height - ballRadius){
-      if(x > paddleX && x < paddleX + paddleWidth){
-        dy = -dy;
-      }else{
-        alert ('Game Over!')
-        document.location.reload();
-      }
   }
+
+  x += dx;
+  y += dy;
 
   if(y + dy > canvas.height - ballRadius){
     if(x > paddleX && x < paddleX + paddleWidth){
@@ -132,7 +145,7 @@ function init(){
     }else{
       // mostra a janela de game over
       document.getElementById('final-score').innerText = score;
-      document.getElementById('game-over').style.display = 'block';
+      gameOverElement.style.display = 'block';
     }
   }
 }
